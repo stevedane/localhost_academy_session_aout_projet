de7d0c76ae11a8558abe67902508921feb5c0637
loic georges <loiciyock95@gmail.com>
2023-11-10 19:44:09 +0100

    update_location_car

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -5,11 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Location extends Model
 {
     use HasFactory;
     protected $fillable=([
 
-        public function 
+        
     ]);
+    public function customer()
+        {
+            return  $this->BelongsTo(customer::class,'id_custommer','id');
+        }
+
+        public function payment()
+        {
+            return  $this->hasOne(payment::class,'id_payment','id');
+        }
 }
