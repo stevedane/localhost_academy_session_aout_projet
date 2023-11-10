3d4a768976a39f48e688ea7ef65011cdbc2c3df4
loic georges <loiciyock95@gmail.com>
2023-11-10 19:19:24 +0100

    update_location_car

--- database/migrations/2023_11_10_171737_create_locations_table.php
+++ database/migrations/2023_11_10_171737_create_locations_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('id_location')->constrained('location','id');
             $table->foreignId('id_customer')->constrained('customer','id');
             $table->foreignId('id_car')->constrained('car','id');
             $table->date('start_date');
