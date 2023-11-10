53c21b5233c71bafbd4a622dee2ce3083905bae1
Maravilla277 <luzmia277@gmail.com>
2023-11-10 19:27:12 +0000

    update model customer  of customer table with hasmany

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
@@ -15,4 +16,9 @@ class Customer extends Model
         'phone_number',
         'history_reservation',
     ];
+
+    public function locations(): HasMany
+    {
+        return $this->hasMany(Location::class);
+    }
 }
