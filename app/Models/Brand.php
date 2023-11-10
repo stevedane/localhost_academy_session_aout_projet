4fe6216a86dd538335f141f2cd95059b65feb6cf
perleeva03 <motaperle141@gmail.com>
2023-11-10 19:14:53 +0100

    update migration file of brand table

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Brand extends Model
 {
     use HasFactory;
-    
+   
 }
