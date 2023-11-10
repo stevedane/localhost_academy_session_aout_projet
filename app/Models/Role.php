661dddb7e3c7193d1a3df9f3ec31c37a69ee493e
Armell <armellmp@gmail.com>
2023-11-10 20:35:11 +0100

    update_table_role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    public function accesses(): hasMany{
+        return $this->hasMany(access::class);
+    }
+    
     use HasFactory;
 }
