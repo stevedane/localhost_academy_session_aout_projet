8755f2ac5cbef084fa69679c21d0d2a134476d4b
cabrel <jeancode1234@gmail.com>
2023-11-10 19:32:20 +0100

    updated_migration_access_table_and_comments_table add

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ([
+       'id_user',
+       'description',
+       'rate'
+    ]);
+
+    public function user(){
+
+        return $this->hasOne(User::class,'id_user','id');
+    }
 }
